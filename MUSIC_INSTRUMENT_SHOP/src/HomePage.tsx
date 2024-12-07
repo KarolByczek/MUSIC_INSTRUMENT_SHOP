6416a3bf538bd3c99d32e4d033bdcc7eba5b820e
KarolByczek <byczek_1986@interia.pl>
2024-12-07 15:26:12 +0100

    Initialisation of the repository.

---
+++ MUSIC_INSTRUMENT_SHOP/src/HomePage.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './HomePage.scss'
+
+function HomePage() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>
+      THE SHOP
+     </h1>
+    </>
+  )
+}
+
+export default HomePage

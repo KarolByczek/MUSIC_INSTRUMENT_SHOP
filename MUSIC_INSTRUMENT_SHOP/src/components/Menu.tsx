188c84d54e7b7e65042e75e34924c7cf76054a9b
KarolByczek <byczek_1986@interia.pl>
2024-12-27 17:08:06 +0100

    ...

--- MUSIC_INSTRUMENT_SHOP/src/components/Menu.tsx
+++ MUSIC_INSTRUMENT_SHOP/src/components/Menu.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Menu = () => {
     return (
         <div className='main_menu'>
             <ul>
-                <Link className="link" to="guitars">
+                <Link className="link" to="/guitars">
                     <div className="menu_link">GUITARS</div>
                 </Link>
-                <Link className="link" to="saxophones">
+                <Link className="link" to="/saxophones">
                     <div className="menu_link">SAXOPHONES</div>
                 </Link>
-                <Link className="link" to="drums">
+                <Link className="link" to="/drums">
                     <div className="menu_link">DRUMS</div>
                 </Link>
-                <Link className="link" to="keyboards">
+                <Link className="link" to="/keyboards">
                     <div className="menu_link">KEYBOARDS</div>
                 </Link>
                 <li>TRUMBTONES</li>

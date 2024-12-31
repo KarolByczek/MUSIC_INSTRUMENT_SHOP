0ef27200fccdc10e44ea77dd1be4ef114bd00825
KarolByczek <byczek_1986@interia.pl>
2024-12-31 11:29:52 +0100

    Further development for the HeadStrip component.

--- MUSIC_INSTRUMENT_SHOP/src/components/HeadStrip.tsx
+++ MUSIC_INSTRUMENT_SHOP/src/components/HeadStrip.tsx
@@ -4,18 +4,15 @@ import Button from './Button.tsx';
 const HeadStrip = () => {
   return (
     <div className='headstrip'>
-      <img src="src/assets/green-gradient-background.jpg" alt="Background of the head strip." />
-      <img src="src/assets/instrument-user-bckg01.jpg" alt="An image of an instrument user." />
-      <img src="src/assets/instrument-user-bckg02.jpg" alt="An image of an instrument user." />
-      <div>
-        <img src="src/assets/exclamatory-fireworks.png" alt="Conspicuous image of fireworks." />
+      <img className='headstrip_subimage left_subimage' src="src/assets/instrument-user-bckg01.jpg" alt="An image of an instrument user." />
+      <div className='headstrip_subimage middle_subimage'>
+        <img  src="src/assets/exclamatory-fireworks.png" alt="Conspicuous image of fireworks." />
         <p>
             This Year's Bestsellers!
         </p>
-      </div>
-      <button>
         <Button className='buy_bestsellers' buttonContent='Buy Now ->'/>
-      </button>
+      </div>
+      <img className='headstrip_subimage right_subimage' src="src/assets/instrument-user-bckg02.jpg" alt="An image of an instrument user." />
     </div>
   )
 }

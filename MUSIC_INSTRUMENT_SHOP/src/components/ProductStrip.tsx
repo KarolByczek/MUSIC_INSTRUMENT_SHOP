18a3d33f01b61850a20f560b1ee18294cb98ecbb
KarolByczek <byczek_1986@interia.pl>
2025-01-03 17:22:55 +0100

    Trying to crack the idea behind the proper usage of svg images...

--- MUSIC_INSTRUMENT_SHOP/src/components/ProductStrip.tsx
+++ MUSIC_INSTRUMENT_SHOP/src/components/ProductStrip.tsx
@@ -16,9 +16,9 @@ const ProductStrip = () => {
             <div className='product_substrip product_substrip_right'>
                 <a href="">EN - &#163;</a>
                 <a href="https://www.thomann.co.uk/mythomann.html" aria-label="Customer Centre" aria-owns="fx-flyin-customer" aria-expanded="false" role="button">
-                    <svg className="fx-icon fx-icon-profile user-navigation__icon fx-size--md2-lg">
-                        <title>customer-center</title>
-                        <use xlinkHref="/static/icons/fxicons.hash-c75f0cbed69a5c255f5f.svg#profile"></use>
+                    <svg className="fx-icon fx-icon-heart user-navigation__icon fx-size--md2-lg">
+                        <title>wishlist</title>
+                        <use xlinkHref="src/assets/cart.svg"></use>
                     </svg>
                 </a>
                 <a href="">&hearts;</a>
